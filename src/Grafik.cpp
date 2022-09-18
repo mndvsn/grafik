e8014bab1c20083a30d079614591e86efbb451d3
mindvision <hello@martinfuruberg.se>
2022-09-18 19:48:39 +0200

    Add glDebugMessageCallback, draw triangles with elements/indices

--- src/Grafik.cpp
+++ src/Grafik.cpp
@@ -9,6 +9,10 @@
 #include <fstream>
 
 #include "utils/File.h"
+#include "utils/GLDebug.h"
+
+
+GLFWwindow* window { nullptr };
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -22,7 +26,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
     glGetShaderiv(id, GL_COMPILE_STATUS, &result);
     if (result == GL_FALSE)
     {
-        int length;
+        int length {};
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
         char* message = static_cast<char*>(alloca(length * sizeof(char)));
         glGetShaderInfoLog(id, length, &length, message);
@@ -53,15 +57,19 @@ static unsigned int CreateShaderProgram(const std::string& vertexShader, const s
     return program;
 }
 
-int main()
+int InitOGL(const char* title, const int width, const int height)
 {
     if (!glfwInit())
     {
         return 1;
     }
+
+#ifdef _DEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+#endif
     
     // Create window and init glfw with context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Grafik", nullptr, nullptr);
+    window = glfwCreateWindow(width, height, title, nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -76,29 +84,55 @@ int main()
         return 1;
     }
 
+#ifdef _DEBUG
     // Print adapter info
     std::cout << "Shading language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
     std::cout << "GL Version: " << glGetString(GL_VERSION) << "\n";
     std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
+#endif
+    
+    return 0;
+}
+
+int main()
+{
+    if (int ret = InitOGL("Grafik", 600, 600); ret != 0)
+    {
+        std::cout << "OpenGL initialization failed!" << std::endl;
+        return ret;
+    }
 
     // Data for triangle
-    constexpr float positions[6] = {
+    constexpr float positions[] =
+    {
        -0.5f, -0.5f,
-        0.0f,  0.5f,
-        0.5f, -0.5f
+        0.5f, -0.5f,
+        0.5f,  0.5f,
+       -0.5f,  0.5f
+    };
+
+    constexpr unsigned int indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
     };
 
     // Generate buffer for static draw
-    unsigned int buffer;
+    unsigned int buffer {};
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     // Define position attribute
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
 
+    unsigned int ibo {};
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    
     File vsFile("src/res/shaders/basic.vs");
     const std::string vertexShader = vsFile.Read();
     
@@ -107,14 +141,27 @@ int main()
     
     const unsigned int shader = CreateShaderProgram(vertexShader, fragmentShader);
     glUseProgram(shader);
+
+#ifdef _DEBUG
+    // Set up debug/error message handling
+    int flags {};
+    glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(HandleGLDebugMessage, nullptr);
+        glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_ERROR, GL_DEBUG_SEVERITY_LOW, 0, nullptr, GL_TRUE);
+    }
+#endif
     
     // Keep loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
         glfwSwapBuffers(window);
 
         glfwPollEvents();
---
+++ src/utils/GLDebug.h
@@ -0,0 +1,9 @@
+﻿/**
+* Grafik 
+ * Copyright 2012-2022 Martin Furuberg 
+ */
+#pragma once
+
+#include <glad/glad.h>
+
+void APIENTRY HandleGLDebugMessage(unsigned int source, unsigned int type, unsigned int id, unsigned int severity, int length, const char* message, const void* userParam);
