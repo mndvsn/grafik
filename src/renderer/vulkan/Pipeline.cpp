4682505fefa69c93a5a9f7c560769be66b692eb2
mindvision <hello@martinfuruberg.se>
2023-02-26 19:14:58 +0100

    Vulkan: Device template class and Pipeline config structs

--- src/renderer/vulkan/Pipeline.cpp
+++ src/renderer/vulkan/Pipeline.cpp
@@ -1,19 +1,29 @@
 ﻿/**
  * Grafik
- * Pipeline
- * Copyright 2023 Martin Furuberg 
+ * Vulkan: Pipeline
+ * Copyright 2023 Martin Furuberg
  */
 #include "gpch.h"
 #include "Pipeline.h"
+
 #include "utils/File.h"
 
+#include <cassert>
 #include <filesystem>
 
 
-Pipeline::Pipeline(const std::string& vertexFile, const std::string& fragmentFile)
-    : _vertexFilePath { vertexFile }, _fragmentFilePath { fragmentFile }
+Pipeline::Pipeline(RenderDevice& device, const std::string& vertexFile, const std::string& fragmentFile, const PipelineConfig& config)
+    : _vertexFilePath { vertexFile }
+    , _fragmentFilePath { fragmentFile }
+    , _device { device }
+    , _config { config }
 {
     _shaderName = ExtractName(vertexFile);
+
+    assert(_config.pipelineLayout != VK_NULL_HANDLE &&
+        "Failed to create Pipeline; Missing PipelineLayout in config");
+    assert(_config.renderPass != VK_NULL_HANDLE &&
+        "Failed to create Pipeline; Missing RenderPass in config");
     
     // Read vertex shader from file as binary
     File vsFile(vertexFile.c_str());
@@ -32,7 +42,73 @@ Pipeline::Pipeline(const std::string& vertexFile, const std::string& fragmentFil
     std::cout << "Size vertex: " << vertexSource->size() << std::endl;
     std::cout << "Size fragment: " << fragmentSource->size() << std::endl;
 
-    // *vertexSource, *fragmentSource
+    createShaderModule(*vertexSource, &_vertShaderModule);
+    createShaderModule(*fragmentSource, &_fragShaderModule);
+
+    VkPipelineShaderStageCreateInfo stages[2];
+    // vertex stage
+    stages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    stages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+    stages[0].module = _vertShaderModule;
+    stages[0].pName = "main";
+    stages[0].flags = 0;
+    stages[0].pNext = nullptr;
+    stages[0].pSpecializationInfo = nullptr;
+    // fragment stage
+    stages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    stages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    stages[1].module = _fragShaderModule;
+    stages[1].pName = "main";
+    stages[1].flags = 0;
+    stages[1].pNext = nullptr;
+    stages[1].pSpecializationInfo = nullptr;
+
+    // vertex buffer interpretation
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo { };
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+    vertexInputInfo.pVertexBindingDescriptions = nullptr;
+
+    VkGraphicsPipelineCreateInfo pipelineInfo { };
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = stages;
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &_config.inputAssemblyInfo;
+    pipelineInfo.pViewportState = &_config.viewportInfo;
+    pipelineInfo.pRasterizationState = &_config.rasterizationInfo;
+    pipelineInfo.pMultisampleState = &_config.multisampleInfo;
+    pipelineInfo.pColorBlendState = &_config.colorBlendInfo;
+    pipelineInfo.pDepthStencilState = &_config.depthStencilInfo;
+    pipelineInfo.pDynamicState = nullptr; // used for setting line width/viewport dynamically
+
+    pipelineInfo.layout = _config.pipelineLayout;
+    pipelineInfo.renderPass = _config.renderPass;
+    pipelineInfo.subpass = _config.subPass;
+
+    // optimization
+    pipelineInfo.basePipelineIndex = -1;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+    if (vkCreateGraphicsPipelines(_device.device(), VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &_graphicsPipeline) != VK_SUCCESS)
+    {
+        throw std::runtime_error { "Failed to create Vulkan Graphics Pipeline!" };
+    }
+}
+
+void Pipeline::createShaderModule(const std::vector<char>& source, VkShaderModule* module)
+{
+    VkShaderModuleCreateInfo createInfo { };
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = source.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(source.data());
+
+    if (vkCreateShaderModule(_device.device(), &createInfo, nullptr, module) != VK_SUCCESS)
+    {
+        throw std::runtime_error { "Failed to create Vulkan Shader Module!" };
+    }
 }
 
 std::string Pipeline::ExtractName(const std::string& filePath) const
@@ -43,5 +119,7 @@ std::string Pipeline::ExtractName(const std::string& filePath) const
 
 Pipeline::~Pipeline()
 {
-    
+    vkDestroyShaderModule(_device.device(), _vertShaderModule, nullptr);
+    vkDestroyShaderModule(_device.device(), _fragShaderModule, nullptr);
+    vkDestroyPipeline(_device.device(), _graphicsPipeline, nullptr);
 }
--- src/renderer/vulkan/Pipeline.h
+++ src/renderer/vulkan/Pipeline.h
@@ -4,8 +4,101 @@
  * Copyright 2023 Martin Furuberg
  */
 #pragma once
-#include <glm/fwd.hpp>
+#include "renderer/vulkan/RenderDevice.h"
 
+
+struct PipelineConfig
+{
+    VkViewport viewport { };
+    VkRect2D scissor { };
+    VkPipelineViewportStateCreateInfo viewportInfo { };
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo { };
+    VkPipelineRasterizationStateCreateInfo rasterizationInfo { };
+    VkPipelineMultisampleStateCreateInfo multisampleInfo { };
+    VkPipelineColorBlendAttachmentState colorBlendAttachment { };
+    VkPipelineColorBlendStateCreateInfo colorBlendInfo { };
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo { };
+    VkPipelineLayout pipelineLayout { nullptr };
+    VkRenderPass renderPass { nullptr };
+    uint32_t subPass { 0 };
+
+    PipelineConfig(uint32_t width, uint32_t height)
+    {
+        // vertex data topology
+        inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+        // transformation between pipeline output and target image
+        viewport.x = viewport.y = 0.0f;
+        viewport.width = static_cast<float>(width);
+        viewport.height = static_cast<float>(height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+        // crops image
+        scissor.offset = { 0, 0 };
+        scissor.extent = { width, height };
+
+        viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewportInfo.viewportCount = 1;
+        viewportInfo.pViewports = &viewport;
+        viewportInfo.scissorCount = 1;
+        viewportInfo.pScissors = &scissor;
+
+        // fragment stage
+        rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterizationInfo.depthClampEnable = VK_FALSE;
+        rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+        rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+        rasterizationInfo.lineWidth = 1.0f;
+        rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+        rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rasterizationInfo.depthBiasEnable = VK_FALSE;
+        rasterizationInfo.depthBiasConstantFactor = 0.0f;
+        rasterizationInfo.depthBiasClamp = 0.0f;
+        rasterizationInfo.depthBiasSlopeFactor = 0.0f;
+
+        // anti-aliasing
+        multisampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampleInfo.sampleShadingEnable = VK_FALSE;
+        multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+        multisampleInfo.minSampleShading = 1.0f;
+        multisampleInfo.pSampleMask = nullptr;
+        multisampleInfo.alphaToCoverageEnable = VK_FALSE;
+        multisampleInfo.alphaToOneEnable = VK_FALSE;
+
+        // blending, combining colors in framebuffer
+        colorBlendAttachment.blendEnable = VK_FALSE;
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
+        colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+        colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+        colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlendInfo.logicOpEnable = VK_FALSE;
+        colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;
+        colorBlendInfo.attachmentCount = 1;
+        colorBlendInfo.pAttachments = &colorBlendAttachment;
+        colorBlendInfo.blendConstants[0] = 0.0f;
+        colorBlendInfo.blendConstants[1] = 0.0f;
+        colorBlendInfo.blendConstants[2] = 0.0f;
+        colorBlendInfo.blendConstants[3] = 0.0f;
+
+        depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+        depthStencilInfo.depthTestEnable = VK_TRUE;
+        depthStencilInfo.depthWriteEnable = VK_TRUE;
+        depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS;
+        depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
+        depthStencilInfo.minDepthBounds = 0.0f;
+        depthStencilInfo.maxDepthBounds = 1.0f;
+        depthStencilInfo.stencilTestEnable = VK_FALSE;
+        depthStencilInfo.front = { };
+        depthStencilInfo.back = { };
+    }
+};
 
 class Pipeline
 {
@@ -13,13 +106,26 @@ class Pipeline
     std::string _shaderName { };
     std::string _vertexFilePath { };
     std::string _fragmentFilePath { };
+    RenderDevice& _device;
+    PipelineConfig _config;
+
+    VkPipeline _graphicsPipeline { nullptr };
+    VkShaderModule _vertShaderModule { nullptr };
+    VkShaderModule _fragShaderModule { nullptr };
 
 public:
-    Pipeline(const std::string& vertexFile, const std::string& fragmentFile);
+    Pipeline(
+        RenderDevice& device,
+        const std::string& vertexFile,
+        const std::string& fragmentFile,
+        const PipelineConfig& config);
     ~Pipeline();
 
     Pipeline(const Pipeline&) = delete;
     void operator=(const Pipeline&) = delete;
+
+    void createShaderModule(const std::vector<char>& source, VkShaderModule* module);
+
 private:
     [[nodiscard]] std::string ExtractName(const std::string& filePath) const;
 };
